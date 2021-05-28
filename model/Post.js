d56f88be4383363f255515ca155108ad92fd5437
ZzLuffyzZ <toan1998vn1@gmail.com>
2021-05-28 10:53:14 +0700

    Create Post.js

---
+++ model/Post.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose')
+
+var postSchema = mongoose.Schema({
+    title: String,
+    imgVideo: String,
+    described: String,
+    like: Array,
+    comment: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Comment'
+    }],
+    space: String,
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+var Post = mongoose.model('Post',postSchema)
+module.exports = Post;
